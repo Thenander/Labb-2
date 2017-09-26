9308588b925647644f2eac640013564ca8a47d56
Thenander <info@microlab.se>
2017-09-26 15:05:32 +0200

    Fast figures - Tänk snabbt

--- Labb 2/Program.cs
+++ Labb 2/Program.cs
@@ -35,58 +35,57 @@ namespace Fast_Figures
 
         static void Main()
         {
-            int rndNoDigits;
-            int[] rndNo = new int[] { 100, 999, 1000, 9999, 10000, 99999, 100000, 999999, 1000000, 9999999, 10000000, 99999999, 10, 99 };
+            int rndNumberDigits;
+            int[] rndNo = new int[] { 100, 999, 1000, 9999, 10000, 99999, 100000, 999999, 1000000, 9999999, 10000000, 99999999 };
             string name = Information.Name();
             // Introduktion och instruktion
             Information.WelcomeMessage(name);
-            // Level 0
-            for (int i = 0; i < 1; i++)
-            {
-                rndNoDigits = RandomNumber(rndNo[12], rndNo[13]);
-                PlayGame(rndNoDigits);
-            }
-            LevelInfo();
+
             // Level 1
             for (int i = 0; i < 3; i++)
             {
-                rndNoDigits = RandomNumber(rndNo[0], rndNo[1]);
-                PlayGame(rndNoDigits);
+                rndNumberDigits = RandomNumber(rndNo[10], rndNo[11]);
+                PlayGame(rndNumberDigits);
             }
-            LevelInfo();
+
             // Level 2
+            LevelInfo();
             for (int i = 0; i < 3; i++)
             {
-                rndNoDigits = RandomNumber(rndNo[2], rndNo[3]);
-                PlayGame(rndNoDigits);
+                rndNumberDigits = RandomNumber(rndNo[2], rndNo[3]);
+                PlayGame(rndNumberDigits);
             }
-            LevelInfo();
+
             // Level 3
+            LevelInfo();
             for (int i = 0; i < 3; i++)
             {
-                rndNoDigits = RandomNumber(rndNo[4], rndNo[5]);
-                PlayGame(rndNoDigits);
+                rndNumberDigits = RandomNumber(rndNo[4], rndNo[5]);
+                PlayGame(rndNumberDigits);
             }
-            LevelInfo();
+
             // Level 4
-            for (int i = 0; i < 4; i++)
-            {
-                rndNoDigits = RandomNumber(rndNo[6], rndNo[7]);
-                PlayGame(rndNoDigits);
-            }
             LevelInfo();
-            // Level 5
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < 3; i++)
             {
-                rndNoDigits = RandomNumber(rndNo[8], rndNo[9]);
-                PlayGame(rndNoDigits);
+                rndNumberDigits = RandomNumber(rndNo[6], rndNo[7]);
+                PlayGame(rndNumberDigits);
             }
-            LevelInfo();
+
             // Level 5
-            for (int i = 0; i < 4; i++)
+            LevelInfo();
+            for (int i = 0; i < 3; i++)
             {
-                rndNoDigits = RandomNumber(rndNo[10], rndNo[11]);
-                PlayGame(rndNoDigits);
+                rndNumberDigits = RandomNumber(rndNo[8], rndNo[9]);
+                PlayGame(rndNumberDigits);
+            }
+
+            // Level 5
+            LevelInfo();
+            for (int i = 0; i < 3; i++)
+            {
+                rndNumberDigits = RandomNumber(rndNo[10], rndNo[11]);
+                PlayGame(rndNumberDigits);
             }
             // Avslut
             Information.ByeByeMessage(name);
